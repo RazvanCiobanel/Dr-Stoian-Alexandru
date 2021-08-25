1dc374b97e746c8ecfd5a63ef185430b6d111885
Razvan-Cosmin Ciobanel <razvanciobanel@protonmail.com>
2021-08-25 23:50:13 +0300

    Components and Pages

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
-
+import Home from './pages/Home';
+import Header from './components/Header';
+import Footer from './components/Footer';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
+      <Header />
+      <Home />
+      <Footer />
      
     </div>
   );
---
+++ src/components/Header.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const Header = () => {
+    return (
+        <header>
+            <nav>
+                <ul>
+
+                </ul>
+            </nav>
+            
+        </header>
+    )
+}
+
+export default Header
